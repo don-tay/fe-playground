17927257b1c692adb312c9bbb258a72b25094144
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-09-09 16:35:42 +0800

    Rectify verify account bug

--- verify-account-ui/script.js
+++ verify-account-ui/script.js
@@ -4,25 +4,27 @@ const codes = document.querySelectorAll('.code');
 codes[0].focus();
 
 codes.forEach((code, idx) => {
-  // ! Try implementing in keypress event instead. Valiadtion of input fields will be different.
-  code.addEventListener('keydown', e => {
-    code.classList.remove('invalid');
-
-    // handle special keys
-    if (e.key === 'Backspace') {
-      return setTimeout(() => codes[Math.max(idx - 1, 0)].focus(), 10);
-    } else if (e.key === 'Tab') {
+  code.addEventListener('input', e => {
+    console.log(e.target.value, e.data, code.value);
+    const isValidInput = code.value >= '0' && code.value <= '9';
+    if (!isValidInput) {
+      code.classList.add('invalid');
+      setTimeout(() => (code.value = e.data), 100);
       return;
     }
 
-    const isValidInput = e.key >= 0 && e.key <= 9;
-    if (isValidInput) {
-      // hack to clear any existing number before adding in new input. 1 input will not have multiple digit
-      setTimeout(() => codes[Math.min(idx + 1, codes.length - 1)].focus(), 10);
-    } else {
-      code.classList.add('invalid');
-    }
+    code.classList.remove('invalid');
     // ensures that each input has at MOST 1 character. Will overwrite old value if attempting to input new value.
-    code.value = code.value && '';
+    code.value = e.data;
+
+    return codes[Math.min(idx + 1, codes.length - 1)].focus();
+  });
+
+  // keydown events to detect and handle specific key press behaviours
+  code.addEventListener('keydown', e => {
+    // handle special keys
+    if (e.key === 'Backspace' && !code.value) {
+      return setTimeout(() => codes[Math.max(idx - 1, 0)].focus(), 10);
+    }
   });
 });
