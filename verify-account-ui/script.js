b4ac50e892f092cb9b18d657f29d35d2331a5470
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-09-09 18:36:46 +0800

    Add arrow key handling and success msg

--- verify-account-ui/script.js
+++ verify-account-ui/script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('.container');
+const successMsg = document.getElementById('success-msg');
 const codes = document.querySelectorAll('.code');
 
 // focus on input on page load
@@ -5,7 +7,10 @@ codes[0].focus();
 
 codes.forEach((code, idx) => {
   code.addEventListener('input', e => {
-    console.log(e.target.value, e.data, code.value);
+    container.classList.remove('success');
+    successMsg.classList.remove('show');
+    code.classList.remove('invalid');
+
     const isValidInput = code.value >= '0' && code.value <= '9';
     if (!isValidInput) {
       code.classList.add('invalid');
@@ -13,18 +18,48 @@ codes.forEach((code, idx) => {
       return;
     }
 
-    code.classList.remove('invalid');
     // ensures that each input has at MOST 1 character. Will overwrite old value if attempting to input new value.
     code.value = e.data;
 
-    return codes[Math.min(idx + 1, codes.length - 1)].focus();
+    // if all fields filled with valid values, set success and unfocus box
+    if (isCodeValid()) {
+      displaySuccessMsg();
+    }
+
+    if (idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
   });
 
   // keydown events to detect and handle specific key press behaviours
   code.addEventListener('keydown', e => {
+    if (e.key === 'ArrowLeft') {
+      return setTimeout(() => codes[Math.max(idx - 1, 0)].focus(), 10);
+    }
+    if (e.key === 'ArrowRight') {
+      return setTimeout(
+        () => codes[Math.min(idx + 1, codes.length - 1)].focus(),
+        10
+      );
+    }
+
     // handle special keys
     if (e.key === 'Backspace' && !code.value) {
       return setTimeout(() => codes[Math.max(idx - 1, 0)].focus(), 10);
     }
   });
 });
+
+const isCodeValid = () => {
+  for (const code of codes) {
+    if (code.value < '0' || code.value > '9') {
+      return false;
+    }
+  }
+  return true;
+};
+
+const displaySuccessMsg = () => {
+  successMsg.classList.add('show');
+  container.classList.add('success');
+};
