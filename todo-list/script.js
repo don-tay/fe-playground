e45cc735ed70dc719fe3714cc58e5f0256c2b2e3
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-10-26 17:20:48 -0400

    Add todo submit delete completed

---
+++ todo-list/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosEl = document.getElementById('todos');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  addTodo(e.target);
+});
+
+function addTodo(todo) {
+  const todoText = todo?.text ?? input.value;
+
+  if (todoText) {
+    const todoEl = document.createElement('li');
+    // add completed class to those with completed todo
+    todo?.completed && todoEl.classList.add('completed');
+
+    todoEl.innerText = todoText;
+
+    // add event listener to the created element
+    todoEl.addEventListener('click', () =>
+      todoEl.classList.toggle('completed')
+    );
+
+    todoEl.addEventListener('contextmenu', e => {
+      // prevent native context menu from appearing on right click
+      e.preventDefault();
+
+      todoEl.remove();
+    });
+
+    todosEl.appendChild(todoEl);
+
+    input.value = '';
+  }
+}
