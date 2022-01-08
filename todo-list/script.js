0b00f0e4f4df9ec73274a6ac95d1d08e1df37003
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-01-08 10:06:56 +0800

    Add localstorage wip

--- todo-list/script.js
+++ todo-list/script.js
@@ -2,6 +2,8 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosEl = document.getElementById('todos');
 
+loadFromStorage();
+
 form.addEventListener('submit', e => {
   e.preventDefault();
 
@@ -11,27 +13,32 @@ form.addEventListener('submit', e => {
 function addTodo(todo) {
   const todoText = todo?.text ?? input.value;
 
-  if (todoText) {
-    const todoEl = document.createElement('li');
-    // add completed class to those with completed todo
-    todo?.completed && todoEl.classList.add('completed');
-
-    todoEl.innerText = todoText;
-
-    // add event listener to the created element
-    todoEl.addEventListener('click', () =>
-      todoEl.classList.toggle('completed')
-    );
-
-    todoEl.addEventListener('contextmenu', e => {
-      // prevent native context menu from appearing on right click
-      e.preventDefault();
-
-      todoEl.remove();
-    });
-
-    todosEl.appendChild(todoEl);
-
-    input.value = '';
+  if (!todoText) {
+    return;
   }
+
+  const todoEl = document.createElement('li');
+  // add completed class to those with completed todo
+  todo?.completed && todoEl.classList.add('completed');
+
+  todoEl.innerText = todoText;
+
+  // add event listener to the created element
+  todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'));
+
+  todoEl.addEventListener('contextmenu', e => {
+    // prevent native context menu from appearing on right click
+    e.preventDefault();
+
+    todoEl.remove();
+  });
+
+  todosEl.appendChild(todoEl);
+
+  input.value = '';
+}
+
+function loadFromStorage() {
+  const listItems = localStorage.getItem('listItems');
+  listItems?.forEach(item => {});
 }
